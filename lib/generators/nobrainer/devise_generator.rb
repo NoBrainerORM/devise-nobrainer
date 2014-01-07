76b197ede39bda44bbda8ad7debd840e919f21e7
Nicolas Viennot <nicolas@viennot.biz>
2014-01-07 01:23:46 -0500

    Added type declarations

--- lib/generators/nobrainer/devise_generator.rb
+++ lib/generators/nobrainer/devise_generator.rb
@@ -25,33 +25,33 @@ module NoBrainer
       def migration_data
 <<RUBY
   ## Database authenticatable
-  field :email,              :default => ""
-  field :encrypted_password, :default => ""
+  field :email,              :type => String, :default => ""
+  field :encrypted_password, :type => String, :default => ""
 
   ## Recoverable
-  field :reset_password_token
-  field :reset_password_sent_at
+  field :reset_password_token,   :type => String
+  field :reset_password_sent_at, :type => Time
 
   ## Rememberable
-  field :remember_created_at
+  field :remember_created_at, :type => Time
 
   ## Trackable
-  field :sign_in_count,      :default => 0
-  field :current_sign_in_at
-  field :last_sign_in_at
-  field :current_sign_in_ip
-  field :last_sign_in_ip
+  field :sign_in_count,      :type => Integer, :default => 0
+  field :current_sign_in_at, :type => Time
+  field :last_sign_in_at,    :type => Time
+  field :current_sign_in_ip, :type => String
+  field :last_sign_in_ip,    :type => String
 
   ## Confirmable
-  # field :confirmation_token
-  # field :confirmed_at
-  # field :confirmation_sent_at
-  # field :unconfirmed_email    # Only if using reconfirmable
+  # field :confirmation_token,   :type => String
+  # field :confirmed_at,         :type => Time
+  # field :confirmation_sent_at, :type => Time
+  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
 
   ## Lockable
-  # field :failed_attempts, :default => 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,                  # Only if unlock strategy is :email or :both
-  # field :locked_at
+  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
+  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
+  # field :locked_at,       :type => Time
 RUBY
       end
     end
